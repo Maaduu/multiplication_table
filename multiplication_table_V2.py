513e855ce98d8b98c1124468403d0026a284df9e
Maaduu <mamadubalde644@yahoo.com>
2019-05-28 05:37:04 +0200

    m

--- multiplication_table_V2.py
+++ multiplication_table_V2.py
@@ -5,7 +5,7 @@ while continuer == 'y':
     number = input('Enter a number: ')
 
     for i in range(1, 11):
-        print(number * i)
+        print('{0} X {1} = {2}'.format(number, i, int(number) * i))
     continuer = input('Do you want to continue? y/n')
 
 print('fin du script.')
